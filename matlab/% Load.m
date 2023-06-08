80e84647775c4cd4b558964bbfa7a6b87badcca1
tuvaluboy <fini.iuni@gmail.com>
2023-06-08 11:53:16 +0800

    Update % Load.m

--- matlab/% Load.m
+++ matlab/% Load.m
@@ -2,7 +2,7 @@
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
 
 % Define folder path
-folderPath = 'folder location';
+folderPath = '';
 
 % Get a list of all .mat files in the folder
 files = dir(fullfile(folderPath, '*.mat'));
@@ -10,6 +10,9 @@ files = dir(fullfile(folderPath, '*.mat'));
 % Define number of channels and sampling rate (replace with your actual values)
 nb_chan = 62;
 srate = 200; % replace with your actual sampling rate
+
+% Channel labels
+chan_labels = {'Fp1', 'Fp2', 'F3', 'F4', 'T3', 'C3', 'Cz', 'C4', 'T4', 'P3', 'Pz', 'P4', 'O1', 'O2'};
 
 % Loop through each file in the folder
 for k = 1:length(files)
@@ -27,6 +30,17 @@ for k = 1:length(files)
 
     % Import data into EEGLAB
     EEG = pop_importdata('dataformat', 'array', 'nbchan', nb_chan, 'data', 'EEG_data', 'srate', srate, 'pnts', 0, 'xmin', 0);
+
+    % Keep only the first 14 channels
+    EEG = pop_select(EEG, 'channel', 1:14);
+
+    % Create a new chanlocs structure with these labels
+    for i = 1:length(chan_labels)
+        EEG.chanlocs(i).labels = chan_labels{i};
+    end
+
+    % Set the standard 10-20 locations
+    EEG = pop_chanedit(EEG, 'lookup','standard-10-5-cap385.elp');
 
     % Update EEGLAB
     [ALLEEG, EEG, CURRENTSET] = eeg_store(ALLEEG, EEG);
